dfb713107bb693919b304da5523e3530998c4360
leechien0710 <chienle07102001@gmail.com>
2023-09-06 10:28:12 +0700

    add submit.java

---
+++ src/main/java/payment/submit.java
@@ -0,0 +1,27 @@
+package payment;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class submit {
+	@GetMapping("/")
+	public String home() {
+		return "NewFile";
+	}
+	@GetMapping("/submit")
+	public String submit(@RequestParam(name="ten") String ten,
+			@RequestParam(name="kieu") String kieu,
+			@RequestParam(name="so") String so,
+			@RequestParam(name="cvc") String cvc,
+			Model model) {
+		RestTemplate restTemplate = new RestTemplate();
+		String serverUrl = "http://localhost:9090/chiphi?ten=" + ten + "&kieu=" + kieu + "&so=" + so + "&cvc=" + cvc;
+	    String response = restTemplate.getForObject(serverUrl, String.class);
+		model.addAttribute("input", response);
+		return "NewFile1";
+	}
+}
